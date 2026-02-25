c09eae241cefaf270a5c02954d1ab1aa1a85186c
George Clinkscales <1lclink2@att.net>
2026-02-25 12:16:08 -0500

    Initial Commit

---
+++ Stamped! A City Passport/Stamped__A_City_PassportApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stamped__A_City_PassportApp.swift
+//  Stamped! A City Passport
+//
+//  Created by George Clinkscales on 2/25/26.
+//
+
+import SwiftUI
+
+@main
+struct Stamped__A_City_PassportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
